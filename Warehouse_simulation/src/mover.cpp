dc5e13c89c8f82bec896651c9562753dc0556633
Akondi Sai Karthik Vyas <vyasakondi009@gmail.com>
2023-10-31 16:44:30 -0400

    Added mover.cpp (code with basic code to publish cmd_vel to husky)

--- Warehouse_simulation/src/mover.cpp
+++ Warehouse_simulation/src/mover.cpp
@@ -1,6 +1,9 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "nav_msgs/Odometry.h"
+#include "geometry_msgs/Twist.h"
+
+
 #include <sstream>
 
 void odom_callback(const nav_msgs::Odometry::ConstPtr& msg)
@@ -11,11 +14,13 @@ void odom_callback(const nav_msgs::Odometry::ConstPtr& msg)
   ROS_INFO("Husky Pose - x: [%f], y: [%f], z: [%f]", x, y, z);
 
 }
-
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "mover");
+
+  
   ros::NodeHandle n;
+  ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/husky_velocity_controller/cmd_vel", 10);
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
   /*Subscriber to get odometry data for husky in Ware house*/
@@ -24,12 +29,18 @@ int main(int argc, char **argv)
   int count = 0;
   while (ros::ok())
   {
-    std_msgs::String msg;
-    std::stringstream ss;
-    // ss << "hello world " << count;
-    // msg.data = ss.str();
-    // ROS_INFO("%s", msg.data.c_str());
-    // chatter_pub.publish(msg);
+        geometry_msgs::Twist vel_msg;
+
+        vel_msg.linear.x = 1.0; 
+        vel_msg.linear.y = 0.0;
+        vel_msg.linear.z = 0.0;
+        vel_msg.angular.x = 0.0;
+        vel_msg.angular.y = 0.0;
+        vel_msg.angular.z = 0.0;
+        vel_pub.publish(vel_msg);
+        ros::spinOnce();
+        loop_rate.sleep();
+
     ros::spinOnce();
     loop_rate.sleep();
     ++count;
@@ -38,3 +49,4 @@ int main(int argc, char **argv)
 
   return 0;
 }
+
