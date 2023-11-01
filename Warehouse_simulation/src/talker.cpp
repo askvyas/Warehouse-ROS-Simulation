d4ef6fbcd80ff7a4c6cf8b9d96e91c0a1d729a46
Akondi Sai Karthik Vyas <vyasakondi009@gmail.com>
2023-11-01 19:25:14 -0400

    Updated README.md

--- Warehouse_simulation/src/talker.cpp
+++ Warehouse_simulation/src/talker.cpp
@@ -17,7 +17,6 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "talker");
   ros::NodeHandle n;
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-
   /*Subscriber to get odometry data for husky in Ware house*/
   ros::Subscriber pose_sub=n.subscribe("/husky_velocity_controller/odom",100,odom_callback);
   ros::Rate loop_rate(10);
