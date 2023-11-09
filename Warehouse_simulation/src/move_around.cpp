977e7d71d91a15b61b5224233e82a1588268235e
Akondi Sai Karthik Vyas <vyasakondi009@gmail.com>
2023-11-08 19:42:27 -0500

    move_around.cpp added and deleted ros navigate plugin

---
+++ Warehouse_simulation/src/move_around.cpp
@@ -0,0 +1,54 @@
+#include "ros/ros.h"
+#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/Twist.h"
+
+class Drive {
+private:
+    ros::NodeHandle n_;
+    ros::Publisher vel_pub_;
+    ros::Subscriber pose_sub_;
+
+    void odom_callback(const nav_msgs::Odometry::ConstPtr& msg) {
+        double x = msg->pose.pose.position.x;
+        double y = msg->pose.pose.position.y;
+        ROS_INFO("Robot Pose - x: [%f], y: [%f]", x, y);
+        move(x, y);
+    }
+
+void move(double robot_x, double robot_y) {
+    geometry_msgs::Twist vel_msg;
+
+
+        if(robot_x>16.9)
+        {
+            vel_msg.angular.z=0.3;
+        }
+        vel_msg.linear.x = 0.5;
+        // vel_msg.angular.z = 0;
+
+    vel_pub_.publish(vel_msg);
+}
+public:
+    Drive()
+        : n_() {
+        vel_pub_ = n_.advertise<geometry_msgs::Twist>("/husky_velocity_controller/cmd_vel", 10);
+        pose_sub_ = n_.subscribe("/husky_velocity_controller/odom", 10, &Drive::odom_callback, this);
+    }
+
+    void run() {
+        ros::Rate loop_rate(10);
+        while (ros::ok()) {
+            ros::spinOnce();
+            loop_rate.sleep();
+        }
+    }
+};
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "drive");
+
+    Drive navigator;
+    navigator.run();
+
+    return 0;
+}
