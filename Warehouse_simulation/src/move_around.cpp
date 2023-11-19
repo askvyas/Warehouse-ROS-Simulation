2a4b61c03f3d40038afb15261fce368ad0756af9
Akondi Sai Karthik Vyas <vyasakondi009@gmail.com>
2023-11-18 19:13:15 -0500

    move_around.cpp updated with code to handle laserscana and removed odometry related code

--- Warehouse_simulation/src/move_around.cpp
+++ Warehouse_simulation/src/move_around.cpp
@@ -2,6 +2,8 @@
 #include "nav_msgs/Odometry.h"
 #include "geometry_msgs/Twist.h"
 #include <sensor_msgs/LaserScan.h>
+#include <unordered_map>
+#include <numeric>
 
 
 class Drive {
@@ -11,33 +13,145 @@ private:
     ros::Subscriber pose_sub_;
     ros::Subscriber scan_sub_;
 
+    std::vector<float> selectArea(const std::vector<float>& arr,int s,int id)
+    {
+        std::vector<float> points;
+        int i=0;
+        int x=0;
+        if(id==0)
+        {
+            i=0;
+            x=180;
+
+        }
+        else if (id==1)
+        {
+            i=180;
+            x=360;
+        }
+        else if (id==2)
+        {
+          i=360;
+            x=540;
+        }
+        else if (id==3)
+        {
+              i=540;
+            x=719;
+        }
+        
+        
+        for(i;i<x;i++)
+        {
+            points.push_back(arr[i]);
+        }
+        return points;
+
+    }
+    float average(std::vector<float> const& v){
+    if(v.empty()){
+        return 0;
+    }
+
+    float sum = std::accumulate(v.begin(), v.end(), 0.0); 
+    return sum / v.size();
+}
+
+    
    
 
-    void odom_callback(const nav_msgs::Odometry::ConstPtr& msg) {
-        double x = msg->pose.pose.position.x;
-        double y = msg->pose.pose.position.y;
-        ROS_INFO("Robot Pose - x: [%f], y: [%f]", x, y);
-        move(x, y);
-    }
+   
 
     void scan_callback(const sensor_msgs::LaserScan::ConstPtr& scan_msg)
 
     {
-    int center_index = scan_msg->ranges.size() / 2;
-    float range = scan_msg->ranges[center_index];
-    ROS_INFO("Range at center: %f", range);
+
+    std::unordered_map< std::string , float> umap; 
+    std::vector<float> ll=selectArea(scan_msg->ranges,720,0);
+    std::vector<float> lc=selectArea(scan_msg->ranges,720,1);
+    std::vector<float> rc=selectArea(scan_msg->ranges,720,2);
+    std::vector<float> rr=selectArea(scan_msg->ranges,720,3);
+    umap["ll"]=average(ll);
+    umap["lc"]=average(ll);
+    umap["rc"]=average(ll);
+    umap["rr"]=average(ll);
+    std::string dir="";
+
+
+    float m=std::max({umap["ll"],umap["lc"],umap["rc"],umap["rr"]});
+
+    for (const auto& x : umap) {
+        if (x.second == m) {
+            dir += x.first;
+        }
+    }
+        ROS_INFO("Max range: %f", m);
+    ROS_INFO("Direction: %s", dir);
+
+
+    if(dir=="ll")
+    {
+        move(0);
+    }
+    else if(dir=="lc")
+    {
+        move(1);
+    }
+    else if(dir=="rc")
+    {
+        move(2);
+    }
+    else if(dir=="rr")
+    {
+        move(3);
+    }
+    else
+    {
+        move(-1);
+    }
+
+    
+
+
+
 
 
     }
  
 
-    void move(double robot_x, double robot_y) {
+    void move(int d) {
         geometry_msgs::Twist vel_msg;
 
-        if(robot_x > 16.9) {
-            vel_msg.angular.z = 0.3;
+        vel_msg.linear.x = 0.7;
+
+        if(d==0)
+        {
+        vel_msg.angular.z = -0.25;
+
         }
-        vel_msg.linear.x = 0.5;
+        else if(d==1)
+        {
+        vel_msg.angular.z = -0.12;
+
+        }
+        else if(d==2)
+        {
+        vel_msg.angular.z = 0.12;
+
+        }
+        else if(d==3)
+        {
+        vel_msg.angular.z = 0.25;
+
+        }
+        if(d==-1)
+        {
+            vel_msg.linear.x = 0.7;
+
+        }
+
+
+
         vel_pub_.publish(vel_msg);
     }
 
@@ -47,7 +161,7 @@ public:
 
 
         vel_pub_ = n_.advertise<geometry_msgs::Twist>("/husky_velocity_controller/cmd_vel", 10);
-        pose_sub_ = n_.subscribe("/husky_velocity_controller/odom", 10, &Drive::odom_callback, this);
+        // pose_sub_ = n_.subscribe("/husky_velocity_controller/odom", 10, &Drive::odom_callback, this);
         scan_sub_ = n_.subscribe("/scan",10,&Drive::scan_callback,this);
 
     }
